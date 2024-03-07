6bd5088960cdd971ae0c29c744acf8f6d36f6178
Chi-Jonathan <jchi@nevada.unr.edu>
2024-03-07 03:53:52 -0500

    linting python

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -74,11 +74,11 @@ def registration(request):
     if not username_exist:
         # Create user in auth_user table
         user = User.objects.create_user(
-        username=username,
-        first_name=first_name, 
-        last_name=last_name,
-        password=password, 
-        email=email)
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
+            email=email)
         # Login the user and redirect to list page
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
@@ -97,7 +97,7 @@ def get_cars(request):
     cars = []
     for car_model in car_models:
         cars.append(
-        {"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+            {"CarModel": car_model.name, "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
 
 
@@ -145,7 +145,8 @@ def add_review(request):
             return JsonResponse({"status": 200})
         except Exception as err:
             print(f"Unexpected {err=}, {type(err)=}")
-            return JsonResponse({"status": 401, 
-                                "message": "Error in posting review"})
+            return JsonResponse(
+                {"status": 401,
+                 "message": "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
